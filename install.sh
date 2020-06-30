719c416b0ea1f4ac3fbfa44d07b6ef1a5daa6df5
MPi3D <matthieu.charette@gmail.com>
2020-06-30 20:06:11 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 set -e
-sudo apt-get install lxappearance pcmanfm xfwm4
+sudo apt-get install xfwm4
 mkdir -p ~/.icons ~/.themes ~/.wallpaper
 cp -r Sweet-Rainbow ~/.icons
 cp -r Sweet-Dark ~/.themes
 cp -r Sweet-Wallpapers ~/.wallpaper
+xfconf-query -c xfwm4 -p /general/theme -s Sweet-Dark
+pcmanfm -w ~/.wallpaper/Sweet-Wallpapers/sweet-space.png
+sed -i 's#^\(sNet/ThemeName\s*=\s*\).*$#\1Sweet-Dark#' ~/.config/lxsession/LXDE-pi/desktop.conf
+sed -i 's#^\(sNet/IconThemeName\s*=\s*\).*$#\1Sweet-Rainbow#' ~/.config/lxsession/LXDE-pi/desktop.conf
+sed -i 's#^\(sGtk/CursorThemeName\s*=\s*\).*$#\1Sweet-Rainbow#' ~/.config/lxsession/LXDE-pi/desktop.conf
+sed -i 's#^\(window_manager\s*=\s*\).*$#\1xfwm4#' ~/.config/lxsession/LXDE-pi/desktop.conf
 echo "Successfully installed Sweet Theme."
-echo "Please configure your GTK, icons and cursor theme by using the command 'lxappearance'"
-echo "To configure your windows theme use the command 'xfwm4-settings'"
-echo "To configure your wallpaper theme use the command 'pcmanfm --desktop-pref'"
