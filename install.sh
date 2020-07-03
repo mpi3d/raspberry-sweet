449d7b45d0b1262da7afdf35a16478ecce18adc1
MPi3D <matthieu.charette@gmail.com>
2020-07-03 17:34:45 +0200

    Created plymouth sweet theme

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 set -e
 sudo apt-get install xfwm4
 mkdir -p ~/.icons ~/.themes ~/.wallpaper
-cp -r Sweet-Rainbow ~/.icons
-cp -r Sweet-Dark ~/.themes
-cp -r Sweet-Wallpapers ~/.wallpaper
+sudo cp -r Sweet-Rainbow /usr/share/icons/Sweet-Rainbow
+sudo cp -r Sweet-Dark /usr/share/themes/Sweet-Dark
+sudo cp -r Sweet-Wallpapers /usr/share/wallpapers/Sweet-Wallpapers
+sudo cp -r Sweet-Theme /usr/share/plymouth/themes/sweet-theme
 xfconf-query -c xfwm4 -p /general/theme -s Sweet-Dark
-pcmanfm -w ~/.wallpaper/Sweet-Wallpapers/sweet-space.png
+pcmanfm -w /usr/share/wallpapers/Sweet-Wallpapers/sweet-space.png
 sed -i 's#^\(sNet/ThemeName\s*=\s*\).*$#\1Sweet-Dark#' ~/.config/lxsession/LXDE-pi/desktop.conf
 sed -i 's#^\(sNet/IconThemeName\s*=\s*\).*$#\1Sweet-Rainbow#' ~/.config/lxsession/LXDE-pi/desktop.conf
 sed -i 's#^\(sGtk/CursorThemeName\s*=\s*\).*$#\1Sweet-Rainbow#' ~/.config/lxsession/LXDE-pi/desktop.conf
