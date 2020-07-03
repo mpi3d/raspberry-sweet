5e4265aae76a937d8f967b3fabad4a7c78e8ffb6
MPi3D <matthieu.charette@gmail.com>
2020-07-03 17:37:34 +0200

    Created plymouth sweet theme

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 set -e
 sudo apt-get install xfwm4
-mkdir -p ~/.icons ~/.themes ~/.wallpaper
-sudo cp -r Sweet-Rainbow /usr/share/icons/Sweet-Rainbow
+sudo cp -r Sweet-Theme /usr/share/plymouth/themes/sweet-theme
 sudo cp -r Sweet-Dark /usr/share/themes/Sweet-Dark
 sudo cp -r Sweet-Wallpapers /usr/share/wallpapers/Sweet-Wallpapers
-sudo cp -r Sweet-Theme /usr/share/plymouth/themes/sweet-theme
+sudo cp -r Sweet-Rainbow /usr/share/icons/Sweet-Rainbow
+sudo plymouth-set-default-theme -R sweet-theme
 xfconf-query -c xfwm4 -p /general/theme -s Sweet-Dark
 pcmanfm -w /usr/share/wallpapers/Sweet-Wallpapers/sweet-space.png
 sed -i 's#^\(sNet/ThemeName\s*=\s*\).*$#\1Sweet-Dark#' ~/.config/lxsession/LXDE-pi/desktop.conf
