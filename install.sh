7cc51d08f647cc82a308b935230d114a47fd282a
MPi3D <matthieu.charette@gmail.com>
2020-09-26 18:23:16 +0200

    Add openbox theme

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
 set -e
-sudo apt-get install xfwm4 -y
-rm -rf ~/.config/lxsession/LXDE-pi
 sudo cp -TR Sweet-Theme /usr/share/plymouth/themes/sweet-theme
 sudo cp -TR Sweet-Dark /usr/share/themes/Sweet-Dark
 sudo cp -TR Sweet-Wallpapers /usr/share/wallpapers/Sweet-Wallpapers
 sudo cp -TR Sweet-Rainbow /usr/share/icons/Sweet-Rainbow
 sudo plymouth-set-default-theme -R sweet-theme
-xfconf-query -c xfwm4 -p /general/theme -s Sweet-Dark
 pcmanfm -w /usr/share/wallpapers/Sweet-Wallpapers/sweet-space.png
-sudo sed -i 's#^\(sNet/ThemeName\s*=\s*\).*$#\1Sweet-Dark#' /etc/xdg/lxsession/LXDE-pi/desktop.conf
-sudo sed -i 's#^\(sNet/IconThemeName\s*=\s*\).*$#\1Sweet-Rainbow#' /etc/xdg/lxsession/LXDE-pi/desktop.conf
-sudo sed -i 's#^\(sGtk/CursorThemeName\s*=\s*\).*$#\1Sweet-Rainbow#' /etc/xdg/lxsession/LXDE-pi/desktop.conf
-sudo sed -i 's#^\(sGtk/ColorScheme\s*=\s*\).*$#\1#' /etc/xdg/lxsession/LXDE-pi/desktop.conf
-sudo sed -i 's#^\(window_manager\s*=\s*\).*$#\1xfwm4#' /etc/xdg/lxsession/LXDE-pi/desktop.conf
 echo "Successfully installed Sweet Theme."
+echo "Please use lxappearance to select the Sweet theme."
