3ca2e69ed1947f31a47b6805f03aecb49ec71af2
MPi3D <matthieu.charette@gmail.com>
2020-10-10 18:10:05 +0200

    Update

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@ sudo cp -TR Sweet-Theme /usr/share/plymouth/themes/sweet-theme
 sudo cp -TR Sweet-Dark /usr/share/themes/Sweet-Dark
 sudo cp -TR Sweet-Wallpapers /usr/share/wallpapers/Sweet-Wallpapers
 sudo cp -TR Sweet-Rainbow /usr/share/icons/Sweet-Rainbow
-sudo plymouth-set-default-theme -R sweet-theme
-pcmanfm -w /usr/share/wallpapers/Sweet-Wallpapers/sweet-space.png
-echo 'Successfully installed Sweet Theme"
+echo 'Successfully installed Sweet Theme'
 echo 'Please use "lxappearance" to change your theme for "Sweet-Dark" theme and "Sweet-Rainbow" icons'
-echo 'Use "plymouth-set-default-theme" your plymouth screen for "sweet-theme"'
-echo 'Use "pcmanfm --desktop-pref" change your wallpaper for "/usr/share/wallpapers/Sweet-Wallpapers"'
+echo 'Use "sudo plymouth-set-default-theme" to change your plymouth screen for "sweet-theme"'
+echo 'Use "pcmanfm --desktop-pref" to change your wallpaper for "/usr/share/wallpapers/Sweet-Wallpapers"'
