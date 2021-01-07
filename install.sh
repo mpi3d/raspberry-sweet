04fcf19800963952ef11e9d7eac0342d2f1866e6
MPi3D <matthieu.charette@gmail.com>
2021-01-07 19:37:39 +0100

    change repository case

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 set -e
-sudo cp -TR Sweet-Theme /usr/share/plymouth/themes/sweet-theme
-sudo cp -TR Sweet-Dark /usr/share/themes/Sweet-Dark
-sudo cp -TR Sweet-Wallpapers /usr/share/wallpapers/Sweet-Wallpapers
-sudo cp -TR Sweet-Rainbow /usr/share/icons/Sweet-Rainbow
+sudo cp -TR sweet-theme /usr/share/plymouth/themes/sweet-theme
+sudo cp -TR sweet-dark /usr/share/themes/Sweet-Dark
+sudo cp -TR sweet-wallpapers /usr/share/wallpapers/Sweet-Wallpapers
+sudo cp -TR sweet-rainbow /usr/share/icons/Sweet-Rainbow
 echo 'Successfully installed Sweet Theme'
 echo 'Please use "lxappearance" to change your theme for "Sweet-Dark" theme and "Sweet-Rainbow" icons'
 echo 'Use "sudo plymouth-set-default-theme" to change your plymouth screen for "sweet-theme"'
